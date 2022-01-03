71d88d7c7addeadc3ac41d70cf0d3ed6f0f902ac
Reshav Kumar <reshavkumar34@gmail.com>
2022-01-03 15:53:33 +0530

    Update Intro.js

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
             <div className="intro__text" data-aos="fade-up" data-aos-duration="1000">
                 <h3>Hi There,</h3>
                 <h1>I'm Reshav</h1>
-                <p>I'm a 3rd year engineering student, I love gaming and Coding stuff.</p>
+                <p>I'm a final year engineering student, I love gaming and Coding stuff.</p>
 
             </div>
             <div className="intro__image" data-aos="fade-down" data-aos-duration="1000">
